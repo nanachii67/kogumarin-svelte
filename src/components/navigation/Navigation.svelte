8d5670d23ae114cc7dd8b79bfd0595820b5e80b5
kogskogskogs <rinkoguma313@gmail.com>
2026-03-07 19:46:23 +0800

    chore: installation of shadcn-svelte

---
+++ src/components/navigation/Navigation.svelte
@@ -0,0 +1,8 @@
+<nav data-testid="navigation" class="navigation">
+	<div class="navigation__header">
+		<slot name="logo" />
+		<slot name="search" />
+	</div>
+
+	<div data-testid="navigation-content" class="navigation__content"></div>
+</nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,59 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div>
+	<section>
+		<h1>Maru</h1>
+		<p>AKA: PpolHi, maruchansquiggle ✏️</p>
+		<p>Birthday: November 7 🎂</p>
+		<p>Place: Bacolod City 📍</p>
+	</section>
+
+	<section>
+		<h2>⚠️ Before You Follow</h2>
+		<p>
+			I share cute visuals from other people — please have a <span>share button</span> . I’m not an artist
+			🎨
+		</p>
+		<p>If your primary language isn’t English or Filipino, please don’t add me 🌐</p>
+		<p>Interact with posts first so I can prioritize adding you 💬</p>
+		<p>
+			Please don’t embed <span>NSFW content</span>. This account stays
+			<span>R13 / T for Teens</span> 🛡️
+		</p>
+	</section>
+
+	<section>
+		<img
+			alt="Hatsune Miku: Colorful Stage"
+			src="https://maruchansquigle.vercel.app/covers/miku.png"
+		/>
+		<h2>Hatsune Miku: Colorful Stage</h2>
+		<ul>
+			<li>Top 200 – This Way That Way Zookeeper Day</li>
+			<li>Top 300 – Friendship Rebuilt</li>
+			<li>Top 400 – Beyond the Dream of That Day</li>
+			<li>Top 500 – To One Day Reach That Flowery Stage</li>
+		</ul>
+	</section>
+
+	<section>
+		<img alt="Stella Sora" src="https://maruchansquigle.vercel.app/covers/stellasora.png" />
+		<h2>Stella Sora</h2>
+		<ul>
+			<li>Chitose</li>
+			<li>Freesia</li>
+			<li>Nazuna</li>
+			<li>Gerie</li>
+			<li>Mistique</li>
+		</ul>
+	</section>
+
+	<section>
+		<img alt="100% Orange Juice" src="https://maruchansquigle.vercel.app/covers/orangejuice.png" />
+		<h2>100% Orange Juice</h2>
+		<ul>
+			<li>Syura</li>
+			<li>Cook</li>
+			<li>Cuties</li>
+			<li>Chris</li>
+		</ul>
+	</section>
+</div>
